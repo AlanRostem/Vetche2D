963306197f1fa1b4264832176c34a57cec9fa667
SausageManZ <42648988+SausageManZ@users.noreply.github.com>
2018-12-28 15:45:26 +0100

    Added a test for the gfx::FillRect function in the sandbox main

--- Game/src/Application.cpp
+++ Game/src/Application.cpp
@@ -28,6 +28,8 @@ void vtc::Draw()
 	sprt->Draw("full", 0, 0.5 ,0.5, 200, 200);
 	plr->Animate("walk", plrAnim);
 	plr->Draw("walk", 350, 183);
+	gfx::FillColor(255, 50, 0, 255);
+	gfx::FillRect(10, 10, 100, 200);
 }
 
 int main()
